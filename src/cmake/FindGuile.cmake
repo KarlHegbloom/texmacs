1ada847f009f23f494e622d8c62f25fae0eae9a9
Karl Hegbloom <karl.hegbloom@gmail.com>
2017-11-16 13:17:47 -0700

    Update config.h.cmake, more work on CMakeLists, FindGuile

--- src/cmake/FindGuile.cmake
+++ src/cmake/FindGuile.cmake
@@ -23,19 +23,38 @@ IF(GUILECONFIG_EXECUTABLE)
 
   # and if the package of interest also exists for guile-config, then
   # get the information
-  IF(NOT _return_VALUE)
+  if(NOT _return_VALUE)
 
-    EXECUTE_PROCESS(COMMAND ${GUILECONFIG_EXECUTABLE}  link 
+    #  -pthread  -lguile -lltdl -lgmp -lcrypt -lm -lltdl
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  link
       OUTPUT_VARIABLE _guileconfig_link )
-    
-    EXECUTE_PROCESS(COMMAND ${GUILECONFIG_EXECUTABLE}  compile 
+
+    # -pthread
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  compile
       OUTPUT_VARIABLE _guileconfig_compile )
 
-    EXECUTE_PROCESS(COMMAND ${GUILECONFIG_EXECUTABLE}  info libdir 
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info libguileinterface # = 21:0:4
+      OUTPUT_VARIABLE _guileconfig_libguileinterface )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info guileversion # = 1.8.8
+      OUTPUT_VARIABLE _guileconfig_guileversion )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info pkgincludedir # = /usr/include/guile
+      OUTPUT_VARIABLE _guileconfig_pkgincludedir )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info pkglibdir # = /usr/lib/guile
+      OUTPUT_VARIABLE _guileconfig_pkglibdir )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info pkgdatadir # = /usr/share/guile
+      OUTPUT_VARIABLE _guileconfig_pkgdatadir )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info includedir # = /usr/include
+      OUTPUT_VARIABLE _guileconfig_includedir )
+
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  info libdir # = /usr/lib
       OUTPUT_VARIABLE _guileconfig_libdir )
 
-
-    EXECUTE_PROCESS(COMMAND ${GUILECONFIG_EXECUTABLE}  "--version"
+    execute_process(COMMAND ${GUILECONFIG_EXECUTABLE}  "--version"
       OUTPUT_VARIABLE _guileconfig_version ERROR_VARIABLE _guileconfig_version )
     
   
@@ -50,8 +69,8 @@ IF(GUILECONFIG_EXECUTABLE)
     STRING(REPLACE "-L" " " _guile_libdirs ${_guile_libdirs_with_prefix} "")
     STRING(REPLACE "-l" " " _guile_lib_list "${_guile_libraries_with_prefix}" )
     STRING(REPLACE "-I" " " _guile_includes "${_guile_includes_with_prefix}" )
-#    SEPARATE_ARGUMENTS(_guile_libdirs)
-    
+    #    SEPARATE_ARGUMENTS(_guile_libdirs)
+
     # MESSAGE(STATUS ${_guile_libraries_with_prefix})
     SET(_guile_libraries "")
 
@@ -70,24 +89,27 @@ IF(GUILECONFIG_EXECUTABLE)
           ENDIF(_guile_tmp_library_${i})
         ENDIF(NOT _guile_flag_library_${i}) 
       ENDIF (i)
-    ENDFOREACH(i)       
-           
-
+    ENDFOREACH(i)
 
     set(Guile_FOUND YES)
     set(Guile_INCLUDE_DIRS ${_guile_includes})
-    set(Guile_LIBRARIES ${_guile_libraries})
-    set(Guile_CFLAGS ${_guile_definitions_with_prefix})
-    set(Guile_LIBDIR )
+    set(Guile_LIBRARIES    ${_guile_libraries})
+    set(Guile_CFLAGS       ${_guile_definitions_with_prefix})
+    set(guileconfig_version)
+    if(_guileconfig_version)
+      string(STRIP ${_guileconfig_version} guileconfig_version)
+    endif(_guileconfig_version)
+    set(Guile_LIBDIR)
     if(_guileconfig_libdir)
       string(STRIP ${_guileconfig_libdir} Guile_LIBDIR)
     endif(_guileconfig_libdir)
 
-    MESSAGE(">>>" "${Guile_INCLUDE_DIRS}")
-    MESSAGE(">>>" "${Guile_LIBRARIES}")
-    MESSAGE(">>>" "${Guile_CFLAGS}")
-    MESSAGE(">>>" "${_guileconfig_version}")
-    MESSAGE(">>>" "${Guile_LIBDIR}")
+    MESSAGE(STATUS "-- Guile_FOUND          : ${Guile_FOUND}")
+    MESSAGE(STATUS "-- Guile_INCLUDE_DIRS   : ${Guile_INCLUDE_DIRS}")
+    MESSAGE(STATUS "-- Guile_LIBRARIES      : ${Guile_LIBRARIES}")
+    MESSAGE(STATUS "-- Guile_CFLAGS         : ${Guile_CFLAGS}")
+    MESSAGE(STATUS "-- guileconfig_version  : ${guileconfig_version}")
+    MESSAGE(STATUS "-- Guile_LIBDIR         : ${Guile_LIBDIR}")
 
   ELSE( NOT _return_VALUE)
 
